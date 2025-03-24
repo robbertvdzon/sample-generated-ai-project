245381af1b56336eecc785e28c9073e25698e959
Robbert vd Zon <robbert.vanderzon@edsn.nl>
2025-03-24 15:53:39 +0100

    OHMS-687: Implemented footer on main page with 'Contact database' text
    (Generated by OLLAMA using model qwen2.5-coder:32b in 71950 ms)

--- generated/src/main/kotlin/com/example/demo/DemoApplication.kt
+++ generated/src/main/kotlin/com/example/demo/DemoApplication.kt
@@ -1,11 +1,1 @@
-package com.example.demo
-
-import org.springframework.boot.autoconfigure.SpringBootApplication
-import org.springframework.boot.runApplication
-
-@SpringBootApplication
-class DemoApplication
-
-fun main(args: Array<String>) {
-    runApplication<DemoApplication>(*args)
-}
+No changes needed.
